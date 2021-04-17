ddd49ecf530d8a73384cb474b98379b2d77f67b1
Nicolas Lopez <nlopez97f@gmail.com>
2021-04-17 14:33:36 -0500

    splash changed

--- lib/src/pages/widgets/slpash_page.dart
+++ lib/src/pages/widgets/slpash_page.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:walksoft_alcaldia_cali_flutter/src/pages/atoms/top_bottom_bars.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: createAppBar(),
-      body: Container(
-        child: Center(
-          child: Text("Try"),
-        ),
+    return Container(
+      child: Center(
+        child: Text('Splash page'),
       ),
-      bottomNavigationBar: createBottomAppBar(0),
     );
   }
 }

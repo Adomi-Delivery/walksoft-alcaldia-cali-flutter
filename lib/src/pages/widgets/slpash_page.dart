8b92be21b08841ee990832d9c8c7ef778979fd23
Nicolas Lopez <nlopez97f@gmail.com>
2021-04-17 11:55:13 -0500

    splash preparation, later will be deleted

--- lib/src/pages/widgets/slpash_page.dart
+++ lib/src/pages/widgets/slpash_page.dart
@@ -3,9 +3,17 @@ import 'package:flutter/material.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Splash page'),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Center(
+          child: Text("Try"),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: IconButton(
+          icon: Icon(Icons.add),
+        ),
       ),
     );
   }
